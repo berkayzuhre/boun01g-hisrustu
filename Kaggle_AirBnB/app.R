cecd797f7fb2b4b0f4d40678892e2b2f978c3445
Faruk Tufekci <faruk.tufekci@trendyol.com>
2020-08-30 21:43:04 +0300

    app

--- Kaggle_AirBnB/app.R
+++ Kaggle_AirBnB/app.R
@@ -18,10 +18,10 @@ data <- readRDS("data/AB_NYC_2019.rds")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
+    
     # Application title
-    titlePanel("AirBnB Interactive Data Analysis"),
-
+    titlePanel("AirBnB Dataset Interactive Data Analysis"),
+    
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
@@ -29,30 +29,49 @@ ui <- fluidPage(
                         "max_price:",
                         min = 1,
                         max = 10000,
-                        value = 50)
+                        value = 50),
+            
+            sliderInput("min_price",
+                        "min_price:",
+                        min = 1,
+                        max = 10000,
+                        value = 200)
         ),
-
+        
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+            tabsetPanel(
+                tabPanel("Plot", plotOutput("location_vs_price")),
+                tabPanel("Table", dataTableOutput("data_table"))
+            )
+            #plotOutput("location_vs_price"),
+            #dataTableOutput("data_table")
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
+    
+    output$location_vs_price <- renderPlot({
         
         ggplot(data, aes(x=neighbourhood_group, y=price)) + 
             geom_point(aes(col=room_type, size=availability_365)) + 
-            ylim(c(0, input$max_price)) + 
+            ylim(c(input$min_price, input$max_price)) + 
             labs(title="Location Vs Price", 
                  subtitle="(availability, listing space types)", 
                  x="Location", 
                  y="Price", 
                  caption = "Source: AirBnB")
     })
+    
+    output$data_table <- renderDataTable(
+        selected <- select(data, neighbourhood_group, neighbourhood, room_type, price, minimum_nights, 
+                           number_of_reviews, last_review, reviews_per_month, calculated_host_listings_count, 
+                           availability_365),
+        z <- tail(names(sort(table(selected$neighbourhood))), 10),
+        selected[selected$neighbourhood %in% z,]
+    )
 }
 
 # Run the application 
